e92623509632b9c7f0e02392332537117798f25f
ertkjern <orjanert@gmail.com>
2021-02-19 09:08:52 +0100

    Added Example to the project

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,15 @@
+## [1.1.0] - Added example file
+
+Added example project to improve pub.dev points. Pretty much the same as in test folder.
+
 ## [1.0.0] - Stable release
 
-## [0.0.4] - Fixed example
+First version tested in a real project
+
+## [0.0.4] - Fixed Test
+
+Added tests to the project
 
 ## [0.0.1] - Initial release.
 
-* TODO: Describe initial release.
+The first version of the package. Not yet tested directly tested from pub.dev
---
+++ example/example.dart
@@ -0,0 +1,18 @@
+import 'package:haversine_distance/haversine_distance.dart';
+
+final startCoordinate = new Location(60.389739, 5.322323);
+final endCoordinate = new Location(60.393032, 5.327248);
+
+final haversineDistance = HaversineDistance();
+
+main() {
+  final distanceInMeter =
+      haversineDistance.haversine(startCoordinate, endCoordinate, Unit.METER);
+
+  print('Distance between start and end coordinate is: ${distanceInMeter} m.');
+
+  final distanceInKm =
+      haversineDistance.haversine(startCoordinate, endCoordinate, Unit.KM);
+
+  print('Distance between start and end coordinate is: ${distanceInKm} km.');
+}
